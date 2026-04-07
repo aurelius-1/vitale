29a8d6180901724944357f7502c8e74d3d43c4f0
aurelius-1 <leaurica92@gmail.com>
2026-04-08 00:30:32 +0300

    update tests for build

--- vitale_app/src/test/java/com/marius/ptr/vitale/VitaleApplicationTests.java
+++ vitale_app/src/test/java/com/marius/ptr/vitale/VitaleApplicationTests.java
@@ -4,23 +4,41 @@ import com.marius.ptr.vitale.domain.Book;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(
-        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
-)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("integration")
+@Import(TestcontainersConfiguration.class)
+@Testcontainers
 class VitaleApplicationTests {
+
+    @Container
+    static GenericContainer<?> minio = new GenericContainer<>(DockerImageName.parse("minio/minio:latest"))
+            .withExposedPorts(9000)
+            .withEnv("MINIO_ROOT_USER", "minioadmin")
+            .withEnv("MINIO_ROOT_PASSWORD", "minioadmin")
+            .withCommand("server /data");
+
+    @DynamicPropertySource
+    static void s3Properties(DynamicPropertyRegistry registry) {
+        registry.add("s3.endpoint", () -> "http://localhost:" + minio.getMappedPort(9000));
+    }
 
     @Autowired
     private WebTestClient webTestClient;
 
     @Test
     void whenPostRequestThenBookCreated() {
-
         Book book = Book.of("1234567890", "Title", "Author", 9.90, null);
 
         webTestClient
@@ -34,5 +52,4 @@ class VitaleApplicationTests {
                     assertThat(actualBook.isbn()).isEqualTo(book.isbn());
                 });
     }
-
 }
